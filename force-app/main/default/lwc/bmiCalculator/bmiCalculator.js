589aecb7d32525fb59a5f91f5fbe3388cf19719c
Leandro Fernandes <leandro.fernandes@partner.alphabet.com>
2021-06-12 18:15:51 +0200

    43 - BMI Calculator Usage of @track decorator

--- force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
+++ force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
@@ -4,26 +4,28 @@ export default class BmiCalculator extends LightningElement {
 
     cardTitle = "BMI Calculator";
 
-    weight;
-    height;
+    @track bmiData = {
+        weight: 0,
+        height: 0,
+        result: 0
+    }
 
-    @track bmi;
 
     onWeightChange(event) {
-        this.weight = parseFloat(event.target.value);
+        this.bmiData.weight = parseFloat(event.target.value);
 
     }
 
     onHeightChange(event) {
-        this.height = parseFloat(event.target.value);
+        this.bmiData.height = parseFloat(event.target.value);
 
     }
 
     calculateBMI() {
         try{
-            this.bmi = this.weight / (this.height * this.height);
+            this.bmiData.result = this.bmiData.weight / (this.bmiData.height * this.bmiData.height);
         } catch(error){
-            this.bmi = undefined;
+            this.bmiData.result = undefined;
         }
     }
 }

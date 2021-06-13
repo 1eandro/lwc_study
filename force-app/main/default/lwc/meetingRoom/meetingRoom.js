dc058cdcd1eb55e541141367bb45bb1a368aa0ee
Leandro Fernandes <leandro.fernandes@partner.alphabet.com>
2021-06-13 11:19:06 +0200

    54 - Using slots

--- force-app/main/default/lwc/meetingRoom/meetingRoom.js
+++ force-app/main/default/lwc/meetingRoom/meetingRoom.js
@@ -2,7 +2,7 @@ import { LightningElement, api } from 'lwc';
 
 export default class MeetingRoom extends LightningElement {
 
-    @api meetingRoomInfo;
+    // @api meetingRoomInfo;
     @api showRoomInfo = false;
 
 }

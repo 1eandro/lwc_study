fdbadca0f3de50ae5e13a9715ada3cf9cd8afdba
Leandro Fernandes <leandro.fernandes@partner.alphabet.com>
2021-06-13 21:21:38 +0200

    59 - Communication between independent components

--- force-app/main/default/lwc/meetingRoom/meetingRoom.js
+++ force-app/main/default/lwc/meetingRoom/meetingRoom.js
@@ -1,13 +1,17 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { fireEvent } from 'c/pubsub';
+import { CurrentPageReference } from 'lightning/navigation';
 
 export default class MeetingRoom extends LightningElement {
 
     @api meetingRoomInfo;
     @api showRoomInfo = false;
+    @wire(CurrentPageReference) pageReference
 
     tileClickHandler() {
         const tileClick = new CustomEvent('tileclick', { detail: this.meetingRoomInfo, bubbles: true })
         this.dispatchEvent(tileClick)
+        fireEvent(this.pageReference, 'pubsubtileclick', this.meetingRoomInfo)
     }
 
 
--- force-app/main/default/lwc/meetingRooms/meetingRooms.js
+++ force-app/main/default/lwc/meetingRooms/meetingRooms.js
@@ -1,6 +1,6 @@
 import { LightningElement, track } from 'lwc';
 
-export default class MeetingRoom extends LightningElement {
+export default class MeetingRooms extends LightningElement {
 
     @track selectedMeetingRoom
 

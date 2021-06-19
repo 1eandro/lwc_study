14a15fd54655ff79215d3fcd08e6bf2211b3cea8
Leandro Fernandes <leandro.fernandes@partner.alphabet.com>
2021-06-19 14:18:23 +0200

    63 - Lightning Data Service View Record

--- force-app/main/default/lwc/accountManagerLDS/accountManagerLDS.js
+++ force-app/main/default/lwc/accountManagerLDS/accountManagerLDS.js
@@ -1,11 +1,17 @@
-import { LightningElement, track } from 'lwc';
-import { createRecord } from 'lightning/uiRecordApi';
+import { LightningElement, track, wire } from 'lwc';
+import { createRecord, getRecord } from 'lightning/uiRecordApi';
 
+const fieldArray = ['Account.Name', 'Account.Phone', 'Account.Website']
 export default class AccountManagerLDS extends LightningElement {
 
     @track accountName;
     @track accountPhone;
     @track accountWebsite;
+
+    @track recordId
+
+    @wire(getRecord, { recordId: '$recordId', fields: fieldArray })
+    accountRecord;
 
     accountNameChangeHandler(event) {
         this.accountName = event.target.value
@@ -33,9 +39,33 @@ export default class AccountManagerLDS extends LightningElement {
 
         createRecord(recordInput).then(response => {
             console.log('Account has been created: ', response.id)
+            this.recordId = response.id
         }).catch(error => {
             console.error('Error in creating account: ', error.body.message)
         })
 
     }
+
+    get retrievedAccountName() {
+        if (this.accountRecord.data) {
+            return this.accountRecord.data.fields.Name.value
+        }
+        return undefined
+    }
+
+    get retrievedAccountPhone() {
+        if (this.accountRecord.data) {
+            return this.accountRecord.data.fields.Phone.value
+        }
+        return undefined
+    }
+
+    get retrievedAccountWebsite() {
+        if (this.accountRecord.data) {
+            return this.accountRecord.data.fields.Website.value
+        }
+        return undefined
+    }
+
+
 }

2abb74b280d45b7d6704c78b46f189995fbc748d
Leandro Fernandes <leandro.fernandes@partner.alphabet.com>
2021-06-19 14:31:29 +0200

    64 - LDS Record View Form and Record Edit Form

---
+++ force-app/main/default/lwc/accountManagerLDSForms/accountManagerLDSForms.js
@@ -0,0 +1,13 @@
+import { LightningElement, track } from 'lwc';
+
+export default class AccountManagerLDSForms extends LightningElement {
+
+    @track recordId
+
+    successHandler(event) {
+        this.recordId = event.detail.id
+    }
+
+
+
+}

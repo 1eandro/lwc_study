0131a1d6242efd39d981cafeddcd0fb14fabb057
Leandro Fernandes <leandro.fernandes@partner.alphabet.com>
2021-07-17 16:36:09 +0200

    102 - Making a Callout from LWC - VSF Project

--- force-app/main/default/lwc/vaccineSlotFinder/vaccineSlotFinder.js
+++ force-app/main/default/lwc/vaccineSlotFinder/vaccineSlotFinder.js
@@ -4,7 +4,26 @@ export default class VaccineSlotFinder extends LightningElement {
 
     centers = []
     dates = []
-    
+
+    connectedCallback() {
+
+        //const endpoint = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pinCode}&date=${formattedDate}`
+
+        this.fetchVaccineSlots()
+    }
+
+
+    async fetchVaccineSlots() {
+        let pinCode = 302004
+        let formattedDate = "17-07-2021"
+        const vaccineSlotRes = await fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pinCode}&date=${formattedDate}`)
+
+        const slotsData = await vaccineSlotRes.json()
+        console.log("ticaracatica")
+        console.log(slotsData)
+
+    }
+
     get hideMessage() {
         return false
     }

ade217a05f140a78496be4044a778bd20f9f5ab5
Leandro Fernandes <leandro.fernandes@partner.alphabet.com>
2021-07-17 16:27:27 +0200

    101 - Create Markup of LWC Component VSF Project

---
+++ force-app/main/default/lwc/vaccineSlotFinder/vaccineSlotFinder.js
@@ -0,0 +1,12 @@
+import { LightningElement } from 'lwc';
+
+export default class VaccineSlotFinder extends LightningElement {
+
+    centers = []
+    dates = []
+    
+    get hideMessage() {
+        return false
+    }
+
+}

a738b8dd4d83ce14d26c6d7ce25f05b98acddec2
Leandro Fernandes <leandro.fernandes@partner.alphabet.com>
2021-07-18 13:27:43 +0200

    105 - Finishing Formatting the Callout Response - VSF Project

--- force-app/main/default/lwc/vaccineSlotFinder/vaccineSlotFinder.js
+++ force-app/main/default/lwc/vaccineSlotFinder/vaccineSlotFinder.js
@@ -1,57 +1,64 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class VaccineSlotFinder extends LightningElement {
 
-    centers = []
-    dates = []
+
+    data = []
+    columns = []
 
     connectedCallback() {
-
         //const endpoint = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pinCode}&date=${formattedDate}`
-
         this.fetchVaccineSlots()
     }
-
 
     async fetchVaccineSlots() {
         let pinCode = 302004
         let formattedDate = "17-07-2021"
         const vaccineSlotRes = await fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pinCode}&date=${formattedDate}`)
 
-
-
         const slotsData = await vaccineSlotRes.json()
-        console.log("ticaracatica")
         this.buildColumnsAndRows(slotsData.centers)
 
     }
 
-    get hideMessage() {
-        return false
-    }
+    buildColumnsAndRows(payload) {
 
-
-    buildColumnsAndRows(data) {
-        //build columns/dadte
-        const dates = new Map()
-        dates.set("name", { label: "Center Name", fieldName: "name", type: "text" })
+        //build columns/date
+        const columns = new Map()
+        columns.set("centerName", { label: "Center Name", fieldName: "centerName", type: "text" })
 
         //build rows/centers
-        const centers = new Map()
-        for (const center of data) {
-            centers.set(center.center_id, { name: center.name })
+        const data = new Map()
+        for (const center of payload) {
+
+            data.set(center.center_id, { centerName: center.name })
+
             for (const session of center.sessions) {
+
                 // destructuring syntax
                 const { date, available_capacity, min_age_limit } = session
 
-                // add date as column in dates map
-                dates.set(date, { label: "Center Name", fieldName: "name", type: "text" })
+                // add date as column in data map
+                columns.set(date, { label: date, fieldName: date, type: "text" })
+
+                // add column value for the row
+                data.get(center.center_id)[date] = `Available Capacity: ${available_capacity}; Min Age: ${min_age_limit}`
+
             }
+
         }
 
-        console.log('dates', dates)
-        console.log('centers', centers)
+        this.data = Array.from(data.values())
+        // console.log('data values: ' + JSON.stringify(this.data, null, 2))
+        this.columns = Array.from(columns.values())
+        // console.log('columns values: ' + JSON.stringify(this.columns, null, 2))
 
     }
+
+    get hasData() {
+        return this.data.length > 0
+    }
+
+
 
 }
